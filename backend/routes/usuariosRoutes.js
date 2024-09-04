7b90ceabd877f678852fc1c4f89bd154a53756ea
Manuelgarcia1 <manuelalejandrogarcia@live.com.ar>
2024-09-04 17:34:25 -0300

    primeras configuraciones backend

--- backend/routes/usuariosRoutes.js
+++ backend/routes/usuariosRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
 const usuariosController = require('../controllers/usuarioController.js');
 const router = express.Router();
-router.get('/usuarios', usuariosController.getAllUsuarios);
+router.get('/clientes', usuariosController.getAllUsuarios);
 module.exports = router;
